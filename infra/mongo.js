6cce2b0bb68db97153a3e20ba05765175994adef
Douglas Batista <douglas.silva@zooxsmart.com>
2022-07-14 13:15:48 -0300

    Puzzle Helper

---
+++ infra/mongo.js
@@ -0,0 +1,48 @@
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+let client;
+
+class ManageDB {
+  static async connect() {
+    client = new MongoClient(process.env.MONGO, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      serverApi: ServerApiVersion.v1,
+    });
+
+    await client.connect();
+    client.db("puzzlehelper");
+    console.log("Database connection established");
+  }
+
+  static insert(collection, data) {
+    return client.db("puzzlehelper").collection(collection).insertOne(data);
+  }
+
+  static async getAll(collection) {
+    const response = await client
+      .db("puzzlehelper")
+      .collection(collection)
+      .find({})
+      .toArray();
+    return response;
+  }
+
+  static async getOne(collection, query) {
+    const response = await client
+      .db("puzzlehelper")
+      .collection(collection)
+      .find(query)
+      .toArray();
+
+    if (response.length === 0) return "Resource not found";
+
+    delete response[0]._id;
+
+    if (response[0].previeUrl === null) delete response[0].previeUrl;
+
+    return response[0];
+  }
+}
+
+module.exports = ManageDB;
---
+++ src/server.js
@@ -0,0 +1,31 @@
+require("dotenv").config();
+const ManageDB = require("../infra/mongo");
+const { Client } = require("discord.js");
+const botFlags = {
+  allowedMentions: {
+    parse: ["users", "roles", "everyone"],
+    repliedUser: true,
+  },
+  intents: [
+    "GUILDS",
+    "GUILD_MEMBERS",
+    "GUILD_MESSAGES",
+    "GUILD_MESSAGE_REACTIONS",
+  ],
+};
+
+class Server {
+  constructor() {
+    this.client = new Client(botFlags);
+    this.start();
+  }
+
+  async start() {
+    await ManageDB.connect(process.env.DB_NAME);
+    require("./functions/functions")(this.client);
+    this.client.once("ready", () => console.log("Online!"));
+    this.client.login(process.env.BOT_TOKEN);
+  }
+}
+
+module.exports = new Server();
