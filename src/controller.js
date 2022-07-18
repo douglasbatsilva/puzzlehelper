2890433a1a073ea2e09ed057e67a2b9952267620
Douglas Batista <douglas.silva@zooxsmart.com>
2022-07-18 13:24:36 -0300

    chore: adiciona validação marretada de canal

--- src/controller.js
+++ src/controller.js
@@ -9,21 +9,22 @@ class Message {
 
   watch(bot) {
     bot.on("messageCreate", async (message) => {
+      if (!message.channelId !== "998625559007477781") return;
       if (!message.content.startsWith(prefix) || message.author.bot) return;
-  
+
       const args = message.content.slice(prefix.length).split(/ +/);
-  
+
       switch (args[1]) {
         case "resources":
           await resourceService.sendResourceList(message);
           break;
-  
+
         case "pedir":
           const msgArgs = args;
           const resourceName = msgArgs.slice(2).join(" ");
           await resourceService.sendResource(resourceName, message);
           break;
-  
+
         default:
           embedService.invalidCommandEmbed(message);
           break;
